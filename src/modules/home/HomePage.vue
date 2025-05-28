717cc4783c117ae8bc0c89cd0d34846ba393c4dc
Alberto_Desastre <armassantanaalberto@gmail.com>
2025-05-28 21:10:43 +0100

    added styles for header

--- src/modules/home/HomePage.vue
+++ src/modules/home/HomePage.vue
@@ -10,11 +10,13 @@
         </div>
       </header>
 
-      <nav>
+      <nav class="navigation-wrapper">
+        <!-- Navegación principal -->
         <div class="main-navigation">
-          <router-link to="/desk">Escritorio</router-link>
+          <router-link to="/desk" tabindex="0">Escritorio</router-link>
           <nav class="dropdown-wrapper">
-            <p>Explorar</p>
+            <!-- tabindex me permite desplegar el dropdown a conveniencia al hacer click en explorar -->
+            <p tabindex="0">Explorar</p>
             <ul>
               <li>
                 <router-link to="/creations">Creaciones</router-link>
@@ -26,6 +28,7 @@
           </nav>
         </div>
 
+        <!-- Buscador y otras opciones -->
         <input type="text" placeholder="Buscar creaciones..." />
 
         <div class="user-navigation">
@@ -118,41 +121,78 @@ import { ref } from "vue";
 .header-wrapper nav {
   background-color: rgb(135, 46, 6);
   color: white;
-  padding: 5px 10px;
+  padding: 0px 10px;
   justify-content: space-between;
   align-items: center;
 }
 
 .header-wrapper nav input {
+  margin: 5px 0px;
   padding: 7px;
   border: none;
   border-radius: 5px;
 }
 
 /* Navegación principal */
+
 .header-wrapper nav .main-navigation,
 .header-wrapper nav .user-navigation {
   font-weight: 600;
   color: #fff;
+}
+.header-wrapper .main-navigation {
+  gap: 0px;
+  position: relative;
+  z-index: 1;
 }
 
 .main-navigation .dropdown-wrapper {
   display: flex;
   flex-direction: column;
   position: relative;
+  padding: 0px;
+}
+
+.main-navigation router-link,
+.main-navigation p {
+  width: 90px;
+  padding: 20px;
+  height: 28px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.main-navigation > :focus-within {
+  background-color: #5d81a3;
 }
 
 .dropdown-wrapper ul {
   position: absolute;
   display: none;
-  left: 20px;
-  bottom: -25px;
+  left: 0px;
+  bottom: -60px;
+  min-width: 90px;
 }
-.dropdown-wrapper:active ul {
+
+.dropdown-wrapper:focus-within ul {
   display: block;
+  background-color: #5d81a3;
 }
 
 .dropdown-wrapper ul li {
+  padding: 0px;
   margin: 2px 0px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 1.5px solid #fff;
+  width: 70%;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.dropdown-wrapper ul li router-link {
+  padding: 0px;
 }
 </style>
