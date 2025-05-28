a8be64dd4864e868a92c3edc9d07e649d18a8d48
Alberto_Desastre <armassantanaalberto@gmail.com>
2025-05-28 23:08:57 +0100

    added CreationCard component and a list of cards

---
+++ src/components/CreationCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="creation-card">
+    <span>¡Colaborativo!</span>
+    <n-card title="Hombre busca sentido">
+      <template #cover>
+        <route-link to="/creation/1">
+          <img src="../../../public/imgs/hombre-busca-sentido.webp" />
+        </route-link>
+      </template>
+      <p><strong>Por</strong> <router-link to="/author/1">Autor 1</router-link></p>
+    </n-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { NCard } from "naive-ui";
+</script>
+
+<style scoped>
+.n-card {
+  position: relative;
+  min-width: 220px;
+  max-width: 250px;
+}
+
+.creation-card span {
+  position: absolute;
+  top: 15px;
+  left: 5px;
+  padding: 3px 7px;
+  font-size: 10px;
+  transform: rotate(-25deg);
+  background-color: lime;
+  z-index: 2;
+  border-radius: 2px;
+}
+
+.creation-card .n-card img {
+  display: block;
+}
+</style>
--- src/modules/home/HomePage.vue
+++ src/modules/home/HomePage.vue
@@ -1,47 +1,63 @@
 <template>
   <section class="home-page">
     <TopHeader />
+
     <section class="creations-display">
       <h2>Continúa escribiendo...</h2>
       <ul class="creations-list">
-        <li>
-          <!-- Display de la creación -->
-          <router-link to="/creation/1">
-            <img src="../../../public/imgs/hombre-busca-sentido.webp" width="100px" height="100px" />
-            <span>¡Colaborativo!</span>
-          </router-link>
-          <h4>Hombre busca sentido</h4>
-          <!-- Otros links de interés -->
-          <router-link to="/creation/1">
-            <p>Parte 1</p>
-          </router-link>
-          <p>Por <router-link to="/author/1">Autor 1</router-link></p>
+        <li class="creation-item">
+          <CreationCard />
         </li>
-        <li>
-          <!-- Display de la creación -->
-          <router-link to="/creation/1">
-            <img src="../../../public/imgs/hombre-busca-sentido.webp" width="100px" height="100px" />
-            <span>¡Colaborativo!</span>
-          </router-link>
-          <h4>Hombre busca sentido</h4>
-          <!-- Otros links de interés -->
-          <router-link to="/creation/1">
-            <p>Parte 1</p>
-          </router-link>
-          <p>Por <router-link to="/author/1">Autor 1</router-link></p>
+        <li class="creation-item">
+          <CreationCard />
         </li>
-        <li>
-          <!-- Display de la creación -->
-          <router-link to="/creation/1">
-            <img src="../../../public/imgs/hombre-busca-sentido.webp" width="100px" height="100px" />
-            <span>¡Colaborativo!</span>
-          </router-link>
-          <h4>Hombre busca sentido</h4>
-          <!-- Otros links de interés -->
-          <router-link to="/creation/1">
-            <p>Parte 1</p>
-          </router-link>
-          <p>Por <router-link to="/author/1">Autor 1</router-link></p>
+        <li class="creation-item">
+          <CreationCard />
+        </li>
+        <li class="creation-item">
+          <CreationCard />
+        </li>
+        <li class="creation-item">
+          <CreationCard />
+        </li>
+        <li class="creation-item">
+          <CreationCard />
+        </li>
+        <li class="creation-item">
+          <CreationCard />
+        </li>
+        <li class="creation-item">
+          <CreationCard />
+        </li>
+      </ul>
+    </section>
+
+    <section class="creations-display">
+      <h2>Últimas novedades</h2>
+      <ul class="creations-list">
+        <li class="creation-item">
+          <CreationCard />
+        </li>
+        <li class="creation-item">
+          <CreationCard />
+        </li>
+        <li class="creation-item">
+          <CreationCard />
+        </li>
+        <li class="creation-item">
+          <CreationCard />
+        </li>
+        <li class="creation-item">
+          <CreationCard />
+        </li>
+        <li class="creation-item">
+          <CreationCard />
+        </li>
+        <li class="creation-item">
+          <CreationCard />
+        </li>
+        <li class="creation-item">
+          <CreationCard />
         </li>
       </ul>
     </section>
@@ -50,6 +66,52 @@
 
 <script setup lang="ts">
 import TopHeader from "../../components/TopHeader.vue";
+import CreationCard from "../../components/CreationCard.vue";
 </script>
 
-<style scoped></style>
+<style scoped>
+.creations-display {
+  margin: 20px 25px;
+}
+
+.creations-display h2 {
+  font-size: 2rem;
+  margin-bottom: 15px;
+}
+
+.creation-item {
+  position: relative;
+}
+
+.creation-item .n-card {
+  position: relative;
+  min-width: 220px;
+  max-width: 250px;
+}
+
+.creation-item span {
+  position: absolute;
+  top: 15px;
+  left: 5px;
+  padding: 3px 7px;
+  font-size: 10px;
+  transform: rotate(-25deg);
+  background-color: lime;
+  z-index: 2;
+  border-radius: 2px;
+}
+
+.creation-item .n-card img {
+  display: block;
+}
+
+.creations-list {
+  display: flex;
+  gap: 20px;
+  /* Esto me habilita un scroll cuando haya demasiadas historias */
+  overflow-x: auto;
+  padding-bottom: 10px;
+  scrollbar-width: thin;
+  scrollbar-color: #ccc #f5f5f5;
+}
+</style>
