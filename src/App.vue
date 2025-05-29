c561719759cef66d8f332dc440ea7da95bd4f48a
Alberto_Desastre <armassantanaalberto@gmail.com>
2025-05-29 10:58:21 +0100

    added first register form

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <!--   <HelloWorld msg="Vite + Vue" /> -->
-  <HomePage></HomePage>
+  <!-- <HomePage></HomePage> -->
+  <LoginForm />
 </template>
 
 <script setup lang="ts">
+import LoginForm from "./modules/auth/LoginForm.vue";
 import HomePage from "./modules/home/HomePage.vue";
 </script>
 
