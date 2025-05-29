be4e8e88766bb7a2a29ad0c3a06fd0417b6f0ed5
Alberto_Desastre <armassantanaalberto@gmail.com>
2025-05-29 12:29:01 +0100

    added scaffolding for collaboration petition

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
   <!--   <HelloWorld msg="Vite + Vue" /> -->
   <!-- <HomePage></HomePage> -->
   <!-- <LoginForm /> -->
-  <RegisterForm />
+  <!-- <RegisterForm /> -->
+  <CollaborationPetitionPopup />
 </template>
 
 <script setup lang="ts">
 import LoginForm from "./modules/auth/RegisterForm.vue";
 import RegisterForm from "./modules/auth/LoginForm.vue";
 import HomePage from "./modules/home/HomePage.vue";
+import CollaborationPetitionPopup from "./modules/creations/collaborations/CollaborationPetitionPopup.vue";
 </script>
 
 <style scoped>
---
+++ src/modules/creations/collaborations/CollaborationPetitionPopup.vue
@@ -0,0 +1,39 @@
+<template>
+  <n-card class="collaboration-popup" title="Solicitud de colaboración">
+    <!--     <template #header-extra> #header-extra </template> -->
+    <section class="collab-preview">
+      <img width="180px" height="180px" src="../../../../imgs/hombre-busca-sentido.webp" />
+      <article>
+        <h3>Estás a punto de pedir una colaboración para:</h3>
+        <p><strong>En buscado de sentido</strong>, de <router-link>@VictorFrankl</router-link></p>
+      </article>
+    </section>
+
+    <template #footer> #footer </template>
+    <template #action> #action </template>
+  </n-card>
+</template>
+
+<script setup lang="ts">
+import { NCard } from "naive-ui";
+</script>
+
+<style scoped>
+.collaboration-popup {
+  max-width: 700px;
+  margin: 20px auto;
+}
+
+.collab-preview {
+  display: flex;
+  gap: 15px;
+}
+
+.collab-preview {
+  font-size: 1.2rem;
+}
+
+.collab-preview h3 {
+  margin-bottom: 20px;
+}
+</style>
