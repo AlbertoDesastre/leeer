0b47fd8aa7c54111c16d9eab7a425d28029344b3
Alberto_Desastre <armassantanaalberto@gmail.com>
2025-05-30 10:07:59 +0100

    separated collab-authors into a separated component

---
+++ src/modules/creations/components/CoAuthorsDisplay.vue
@@ -0,0 +1,52 @@
+<template>
+  <article class="co-authors-display">
+    <img
+      v-for="(img, index) in images"
+      :key="index"
+      :class="'coauthor-' + (index + 1)"
+      :src="img"
+      alt="Coautor"
+    />
+  </article>
+</template>
+
+<script setup lang="ts">
+// Componente visual para mostrar los avatares de los coautores solapados
+import { defineProps } from "vue";
+const props = defineProps<{ images: string[] }>();
+</script>
+
+<style scoped>
+.co-authors-display {
+  position: relative;
+  padding-top: 15px;
+  margin-bottom: 55px;
+}
+.co-authors-display img {
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  position: absolute;
+  z-index: 10;
+}
+.co-authors-display .coauthor-2 {
+  opacity: 0.9;
+  left: 40px;
+  z-index: 9;
+}
+.co-authors-display .coauthor-3 {
+  opacity: 0.8;
+  left: 80px;
+  z-index: 8;
+}
+.co-authors-display .coauthor-4 {
+  opacity: 0.55;
+  left: 110px;
+  z-index: 7;
+}
+.co-authors-display .coauthor-5 {
+  opacity: 0.4;
+  left: 140px;
+  z-index: 6;
+}
+</style>
--- src/modules/creations/components/CreationDetails.vue
+++ src/modules/creations/components/CreationDetails.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="creation-details">
     <n-card>
+      <!-- Información de la obra -->
       <template #cover>
         <route-link to="/creation/1">
           <img src="../../../../imgs/hombre-busca-sentido.webp" />
@@ -10,16 +11,12 @@
       <p>
         Por <strong><router-link to="/author/1">Autor 1</router-link></strong>
       </p>
+      <!-- Colaboradores -->
       <template #footer>
         <p>¡Ya han colaborado en esta creación X usuarios!</p>
-        <article class="co-authors-display">
-          <img class="coauthor-1" src="../../../../imgs/gato-escritor.png" />
-          <img class="coauthor-2" src="../../../../imgs/gato-escritor.png" />
-          <img class="coauthor-3" src="../../../../imgs/gato-escritor.png" />
-          <img class="coauthor-4" src="../../../../imgs/gato-escritor.png" />
-          <img class="coauthor-5" src="../../../../imgs/gato-escritor.png" />
-        </article>
+        <CoAuthorsDisplay :images="coauthorImages" />
       </template>
+      <!-- Botón para colaborar -->
       <template #action>
         <n-space justify="center">
           <n-button round color="#5d81a3"> Colaborar en esta historia </n-button>
@@ -31,6 +28,15 @@
 
 <script setup lang="ts">
 import { NCard, NButton, NSpace } from "naive-ui";
+import CoAuthorsDisplay from "../components/CoAuthorsDisplay.vue";
+
+const coauthorImages = [
+  "../../../../imgs/gato-escritor.png",
+  "../../../../imgs/gato-escritor.png",
+  "../../../../imgs/gato-escritor.png",
+  "../../../../imgs/gato-escritor.png",
+  "../../../../imgs/gato-escritor.png",
+];
 </script>
 
 <style scoped>
@@ -41,45 +47,6 @@ import { NCard, NButton, NSpace } from "naive-ui";
 
 .creation-details h2 {
   font-size: 1.8rem;
-}
-
-.creation-details .co-authors-display {
-  position: relative;
-  padding-top: 15px;
-  margin-bottom: 55px;
-}
-
-.creation-details .co-authors-display img {
-  width: 60px;
-  height: 60px;
-  border-radius: 50%;
-}
-
-.co-authors-display img {
-  position: absolute;
-  z-index: 10;
-}
-
-.co-authors-display .coauthor-2 {
-  opacity: 0.9;
-  left: 40px;
-  z-index: 9;
-}
-
-.co-authors-display .coauthor-3 {
-  opacity: 0.8;
-  left: 80px;
-  z-index: 8;
-}
-.co-authors-display .coauthor-4 {
-  opacity: 0.55;
-  left: 110px;
-  z-index: 7;
-}
-.co-authors-display .coauthor-5 {
-  opacity: 0.4;
-  left: 140px;
-  z-index: 6;
 }
 
 .creation-details p,
