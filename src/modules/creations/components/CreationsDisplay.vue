fcb3f06b5f4462ad17d83f7da855f48d8a499c04
Alberto_Desastre <armassantanaalberto@gmail.com>
2025-05-30 22:05:44 +0100

    splitted CreationsDisplay into its own component

---
+++ src/modules/creations/components/CreationsDisplay.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="creations-display">
+    <ul class="creations-list">
+      <li v-for="index in 5" class="creation-item">
+        <CreationCard />
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script setup lang="ts">
+import CreationCard from "../../common/components/CreationCard.vue";
+</script>
+
+<style scoped>
+.creations-display {
+  margin: 20px 25px;
+}
+
+.creations-display h2 {
+  font-size: 2rem;
+  margin-bottom: 15px;
+}
+
+.creation-item {
+  position: relative;
+}
+
+.creation-item .n-card {
+  position: relative;
+  min-width: 220px;
+  max-width: 250px;
+}
+
+.creation-item span {
+  position: absolute;
+  top: 15px;
+  left: 5px;
+  padding: 3px 7px;
+  font-size: 10px;
+  transform: rotate(-25deg);
+  background-color: lime;
+  z-index: 2;
+  border-radius: 2px;
+}
+
+.creation-item .n-card img {
+  display: block;
+}
+
+.creations-list {
+  display: flex;
+  gap: 20px;
+  /* Esto me habilita un scroll cuando haya demasiadas historias */
+  overflow-x: auto;
+  padding-bottom: 10px;
+  scrollbar-width: thin;
+  scrollbar-color: #ccc #f5f5f5;
+}
+</style>
--- src/modules/home/pages/HomePage.vue
+++ src/modules/home/pages/HomePage.vue
@@ -5,71 +5,16 @@
       <img class="home-banner" src="../../../../imgs/gato-encima-de-libros.png" />
     </div>
 
-    <section class="creations-display">
-      <h2>Continúa escribiendo...</h2>
-      <ul class="creations-list">
-        <li class="creation-item">
-          <CreationCard />
-        </li>
-        <li class="creation-item">
-          <CreationCard />
-        </li>
-        <li class="creation-item">
-          <CreationCard />
-        </li>
-        <li class="creation-item">
-          <CreationCard />
-        </li>
-        <li class="creation-item">
-          <CreationCard />
-        </li>
-        <li class="creation-item">
-          <CreationCard />
-        </li>
-        <li class="creation-item">
-          <CreationCard />
-        </li>
-        <li class="creation-item">
-          <CreationCard />
-        </li>
-      </ul>
-    </section>
+    <h2>¡Últimas novedades!</h2>
+    <CreationsDisplay />
 
-    <section class="creations-display">
-      <h2>Últimas novedades</h2>
-      <ul class="creations-list">
-        <li class="creation-item">
-          <CreationCard />
-        </li>
-        <li class="creation-item">
-          <CreationCard />
-        </li>
-        <li class="creation-item">
-          <CreationCard />
-        </li>
-        <li class="creation-item">
-          <CreationCard />
-        </li>
-        <li class="creation-item">
-          <CreationCard />
-        </li>
-        <li class="creation-item">
-          <CreationCard />
-        </li>
-        <li class="creation-item">
-          <CreationCard />
-        </li>
-        <li class="creation-item">
-          <CreationCard />
-        </li>
-      </ul>
-    </section>
+    <h2>Lo último de: @Autor</h2>
+    <CreationsDisplay />
   </section>
 </template>
 
 <script setup lang="ts">
-import CreationCard from "../../common/components/CreationCard.vue";
-import TopHeader from "../../common/components/TopHeader.vue";
+import CreationsDisplay from "../../creations/components/CreationsDisplay.vue";
 </script>
 
 <style scoped>
@@ -92,48 +37,9 @@ import TopHeader from "../../common/components/TopHeader.vue";
   width: 500px;
 }
 
-.creations-display {
-  margin: 20px 25px;
-}
-
-.creations-display h2 {
+.home-page h2 {
   font-size: 2rem;
+  margin-left: 25px;
   margin-bottom: 15px;
-}
-
-.creation-item {
-  position: relative;
-}
-
-.creation-item .n-card {
-  position: relative;
-  min-width: 220px;
-  max-width: 250px;
-}
-
-.creation-item span {
-  position: absolute;
-  top: 15px;
-  left: 5px;
-  padding: 3px 7px;
-  font-size: 10px;
-  transform: rotate(-25deg);
-  background-color: lime;
-  z-index: 2;
-  border-radius: 2px;
-}
-
-.creation-item .n-card img {
-  display: block;
-}
-
-.creations-list {
-  display: flex;
-  gap: 20px;
-  /* Esto me habilita un scroll cuando haya demasiadas historias */
-  overflow-x: auto;
-  padding-bottom: 10px;
-  scrollbar-width: thin;
-  scrollbar-color: #ccc #f5f5f5;
 }
 </style>
